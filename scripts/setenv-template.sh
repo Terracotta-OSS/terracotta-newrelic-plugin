d7d0523341bf37dcd1427703b3fad67bdd55d3c9
Fabien Sanglier <fabiensanglier@gmail.com>
2014-04-02 23:07:03 -0400

    adding a setenv.sh capability

---
+++ scripts/setenv-template.sh
@@ -0,0 +1,5 @@
+#Set extra JVM OPTS here by setting PLUGIN_OPTS
+
+#PLUGIN_OPTS="${PLUGIN_OPTS} -Djavax.net.ssl.trustStore=some_newlic_cert.jks -Djavax.net.ssl.trustStorePassword=some_password"
+#PLUGIN_OPTS="${PLUGIN_OPTS} -Dhttp.proxyHost=some_proxy_ip -Dhttp.proxyPort=some_proxy_port"
+#PLUGIN_OPTS="${PLUGIN_OPTS} -Dcom.newrelic.plugins.terracotta.learningmode=true"
