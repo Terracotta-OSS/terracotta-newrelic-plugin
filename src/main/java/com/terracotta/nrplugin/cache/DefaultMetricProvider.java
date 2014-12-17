024754fcc03c9951f0bc154b670ad0349cffe4eb
jeffsegal <jeffrey.segal@gmail.com>
2014-12-17 15:33:49 -0500

    Added rollups for Terracotta and Ehcache NR Components

--- src/main/java/com/terracotta/nrplugin/cache/DefaultMetricProvider.java
+++ src/main/java/com/terracotta/nrplugin/cache/DefaultMetricProvider.java
@@ -19,7 +19,9 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created with IntelliJ IDEA.
@@ -118,6 +120,11 @@ public class DefaultMetricProvider implements MetricProvider {
 			}
 		}
 
+		// Get rollup metrics
+		for (Component component : createRollups(componentMap)) {
+			componentMap.put(component.getName(), component);
+		}
+
 		payload.setAgent(new Agent(hostname, pid, version));
 		payload.setComponents(new ArrayList<Component>(componentMap.values()));
 		log.info("Returning " + numMetrics + " metric(s) from cache.");
@@ -132,5 +139,56 @@ public class DefaultMetricProvider implements MetricProvider {
 						ehcacheAgentGuid : terracottaAgentGuid;
 		return new Component(componentPrefix + "_" + metricDataset.getComponentName(), guid, (long) durationSeconds);
 	}
+
+	public List<Component> createRollups(Map<String, Component> componentMap) {
+		// Maps guid -> metric -> stats
+		Map<String, Map<String, Map<String, Number>>> guids = new ConcurrentHashMap<String, Map<String, Map<String, Number>>>();
+		for (Map.Entry<String, Component> entry : componentMap.entrySet()) {
+			String guid = entry.getValue().getGuid();
+			Map<String, Map<String, Number>> metrics = guids.get(guid);
+			if (metrics == null) {
+				metrics = new ConcurrentHashMap<String, Map<String, Number>>();
+				guids.put(guid, metrics);
+			}
+			for (Map.Entry<String, Object> metricEntry : entry.getValue().getMetrics().entrySet()) {
+				String metricName = metricEntry.getKey();
+				Map<String, Number> rollup = metrics.get(metricName);
+				if (rollup == null) {
+					rollup = new ConcurrentHashMap<String, Number>();
+					metrics.put(metricName, rollup);
+				}
+				Map<String, Number> data = (Map<String, Number>) metricEntry.getValue();
+				addData(rollup, data);
+			}
+		}
+
+		List<Component> rollupComponents = new ArrayList<Component>();
+		for (Map.Entry<String, Map<String, Map<String, Number>>> entry : guids.entrySet()) {
+			double durationSeconds = durationMillis * 0.001;
+			String guid = entry.getKey();
+			String[] guidSplit = guid.split("\\.");
+			String guidSuffix = guidSplit[guidSplit.length - 1];
+			Component component = new Component(componentPrefix + "_" + guidSuffix + "_Rollup", guid, (long) durationSeconds);
+			Map<String, Map<String, Number>> metrics = entry.getValue();
+			for (Map.Entry<String, Map<String, Number>> metric : metrics.entrySet()) {
+				component.putMetric(metric.getKey(), metric.getValue());
+			}
+			rollupComponents.add(component);
+		}
+
+		return rollupComponents;
+	}
+
+	private void addData(Map<String, Number> metrics, Map<String, Number> data) {
+		for (Map.Entry<String, Number> entry : data.entrySet()) {
+			String stat = entry.getKey();
+			Number incr = entry.getValue();
+			Double initial = metrics.get(stat) != null ? (Double) metrics.get(stat) : 0;
+			if (incr != null) {
+				metrics.put(stat, initial + incr.doubleValue());
+			}
+		}
+	}
+
 
 }
